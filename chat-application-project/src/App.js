0f24970cf7dd7b7559ec14a10c4f42bb8766fec2
Hina <hinakouser152@gmail.com>
2023-04-21 11:56:17 +0530

    new app

---
+++ chat-application-project/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// import {ChatEngine} from 'react-chat-engine';
+import Data from './Components/Data';
+
+//Using arrow function component
+const App = () => {
+  return (
+    // <ChatEngine
+    //   userName='Hina'//Put your userName instead
+    //   projectID = '52678c8c-bcd7-4385-8439-dc0b1e6ef521'// Your project id goes here
+    //   userSecret='123456'// Replace with your secret key
+    //  />
+    <Data/>
+  )
+}
+export default App
